dd3c4bcdf65c671728d95d760d887e6403819ecb
Dipak Panpatil <150575213+Dipaksp@users.noreply.github.com>
2023-12-18 13:32:03 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <nav className="flex justify-between items-center p-5 lg:px-10 bg-green-900 text-white">
+      <nav className="flex justify-between items-center p-5 lg:px-10 bg-green-900 nvbar text-white">
         <div className="flex justify-between gap-10">
             <div className="font-bold text-xl">Recco</div>
         <div className="hidden md:flex justify-evenly space-x-4">
@@ -17,7 +17,7 @@ const Navbar = () => {
         </div>
         
         </div>
-        <div className="flex space-x-4">
+        <div className="flex nvbar space-x-4">
           <div className="cursor-pointer hover:text-gray-300"><PiShoppingCartSimpleBold size={24}/></div>
           <div className="cursor-pointer hover:text-gray-300">User Info</div>
           <div className="md:hidden">
